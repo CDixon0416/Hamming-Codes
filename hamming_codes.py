28c78ceced10f7c2b8268f18d0b7dde66c6657c4
Casey <caseyericdixon@gmail.com>
2020-09-27 15:09:29 -0400

    Complete message_to_ham function
    input = 00110001110
    ->0 011 000 1110
    one = 0
    two = 1
    four = 1
    eight = 1
    zero = (one)(two)0 (four)011 (eight)000 1110
    -> 010 1011 1000 1110
    ham = (zero)010 1011 1000 1110
    |0|0|1|0|
    |1|0|1|1|
    |1|0|0|0|
    |1|1|1|0|

--- hamming_codes.py
+++ hamming_codes.py
@@ -9,19 +9,16 @@ def message_to_ham(message):
     #4 = row 2 and row 4
     #8 = row 3 and row 4
     #0 = everything i
-    message_as_int = [] 
-    for element in range(0, (len(message))):
-        message_as_int.append(int(message[element]))
-    print(message) 
     one = message[0] + message[1] + message[3] + message[4] + message[6] + message[8] + message[10] 
-    print(one)
     two = message[0] + message[2] + message[3] + message[5] + message[6] + message[9] + message[10] 
-    print(two)
     four = message[1] + message[2] + message[3] + message[7] + message[8] + message[9] + message[10]   
-    print(four)
     eight = message[4] + message[5] + message[6] + message[7] + message[8] + message[9] + message[10]    
-    print(eight)
-    ham = ""
+    one = determine_bit(one)
+    two = determine_bit(two)
+    four = determine_bit(four)
+    eight = determine_bit(eight)
+    zero = one + two + message[0] + four + message[1:4] + eight + message[4:11]
+    ham = determine_bit(zero) + zero 
     matrix_pretty_print(ham)    
     
 def ham_to_message(ham):
@@ -37,6 +34,16 @@ def matrix_pretty_print(ham):
     print(bar + bar.join(ham[4:8]) + bar)
     print(bar + bar.join(ham[8:12]) + bar)
     print(bar + bar.join(ham[12:16]) + bar)
+
+def determine_bit(message_stub):
+    sum = 0
+    for bit in message_stub:
+        sum += int(bit)
+    if sum %2 == 0:
+        parity = "0"
+    else:
+        parity = "1" 
+    return parity
 
 def main():
     parser = argparse.ArgumentParser(description="""
